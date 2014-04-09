53c8a1744e69bc5636eec90a959cf9bc0cce400f
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-04-09 15:41:54 -0400

    add a menu screen

--- gen/course/examples/touch/ViewTransitions/R.java
+++ gen/course/examples/touch/ViewTransitions/R.java
@@ -10,26 +10,42 @@ package course.examples.touch.ViewTransitions;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int myne=0x7f050000;
+    }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int cbutton=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int mynelogo=0x7f020002;
+        public static final int survey_icon=0x7f020003;
+        public static final int survey_icon_pressed=0x7f020004;
     }
     public static final class id {
-        public static final int include1=0x7f050005;
-        public static final int include2=0x7f050004;
-        public static final int seekBar1=0x7f050001;
-        public static final int seekBar2=0x7f050007;
-        public static final int textView1=0x7f050000;
-        public static final int textView1a=0x7f050002;
-        public static final int textView2=0x7f050006;
-        public static final int textView2a=0x7f050008;
-        public static final int view_flipper=0x7f050003;
+        public static final int button1=0x7f070003;
+        public static final int include1=0x7f070006;
+        public static final int include2=0x7f070005;
+        public static final int seekBar1=0x7f070001;
+        public static final int seekBar2=0x7f070009;
+        public static final int splashscreen=0x7f07000b;
+        public static final int textView1=0x7f070000;
+        public static final int textView1a=0x7f070002;
+        public static final int textView2=0x7f070008;
+        public static final int textView2a=0x7f07000a;
+        public static final int topRow=0x7f070007;
+        public static final int view_flipper=0x7f070004;
     }
     public static final class layout {
         public static final int first=0x7f030000;
         public static final int main=0x7f030001;
-        public static final int second=0x7f030002;
+        public static final int menu=0x7f030002;
+        public static final int second=0x7f030003;
+        public static final int splash=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f060000;
+    }
+    public static final class xml {
+        public static final int progress_drawable=0x7f040000;
+        public static final int thumb_drawable=0x7f040001;
     }
 }
