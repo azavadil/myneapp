42667ce3efc4c6a6aca885e3dbf84d3860702734
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-04-12 14:32:26 -0400

    add a menu label

--- gen/course/examples/touch/ViewTransitions/R.java
+++ gen/course/examples/touch/ViewTransitions/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int default_black=0x7f050001;
         public static final int myne=0x7f050000;
     }
     public static final class dimen {
@@ -25,43 +26,60 @@ public final class R {
     }
     public static final class drawable {
         public static final int cbutton=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int mynelogo=0x7f020002;
-        public static final int survey_icon=0x7f020003;
-        public static final int survey_icon_pressed=0x7f020004;
+        public static final int heart_graphic_empty=0x7f020001;
+        public static final int heart_graphic_filled=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int mynelogo=0x7f020004;
+        public static final int progress0=0x7f020005;
+        public static final int progress1=0x7f020006;
+        public static final int progress2=0x7f020007;
+        public static final int progress3=0x7f020008;
+        public static final int progress4=0x7f020009;
+        public static final int ratebar_theme=0x7f02000a;
+        public static final int ratingbar_full=0x7f02000b;
+        public static final int ratingbar_full_empty=0x7f02000c;
+        public static final int ratingbar_full_filled=0x7f02000d;
+        public static final int survey_icon=0x7f02000e;
+        public static final int survey_icon_pressed=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000d;
-        public static final int button1=0x7f090003;
-        public static final int include1=0x7f090006;
-        public static final int include2=0x7f090005;
-        public static final int menuRows=0x7f090007;
-        public static final int seekBar1=0x7f090001;
-        public static final int seekBar2=0x7f090009;
-        public static final int splashscreen=0x7f09000b;
-        public static final int survey_button=0x7f09000c;
-        public static final int textView1=0x7f090000;
-        public static final int textView1a=0x7f090002;
-        public static final int textView2=0x7f090008;
-        public static final int textView2a=0x7f09000a;
-        public static final int view_flipper=0x7f090004;
+        public static final int action_settings=0x7f0a000d;
+        public static final int include1=0x7f0a0006;
+        public static final int include2=0x7f0a0005;
+        public static final int menu_activity=0x7f0a0007;
+        public static final int rating_bar_1=0x7f0a0000;
+        public static final int seekBar1=0x7f0a0002;
+        public static final int seekBar2=0x7f0a0009;
+        public static final int splashscreen=0x7f0a000b;
+        public static final int survey_button=0x7f0a000c;
+        public static final int textView1=0x7f0a0001;
+        public static final int textView1a=0x7f0a0003;
+        public static final int textView2=0x7f0a0008;
+        public static final int textView2a=0x7f0a000a;
+        public static final int view_flipper=0x7f0a0004;
     }
     public static final class layout {
         public static final int first=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int menu=0x7f030002;
-        public static final int second=0x7f030003;
-        public static final int splash=0x7f030004;
-        public static final int survey_button=0x7f030005;
+        public static final int graphic_label=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int menu=0x7f030003;
+        public static final int progress_graphic=0x7f030004;
+        public static final int second=0x7f030005;
+        public static final int splash=0x7f030006;
+        public static final int survey_button=0x7f030007;
     }
     public static final class menu {
-        public static final int menu=0x7f080000;
+        public static final int menu=0x7f090000;
     }
     public static final class string {
         public static final int action_settings=0x7f070002;
         public static final int app_name=0x7f070000;
         public static final int hello_world=0x7f070003;
         public static final int title_activity_menu=0x7f070001;
+    }
+    public static final class style {
+        public static final int labelTextAppearance=0x7f080001;
+        public static final int progressRatingBar=0x7f080000;
     }
     public static final class xml {
         public static final int progress_drawable=0x7f040000;
